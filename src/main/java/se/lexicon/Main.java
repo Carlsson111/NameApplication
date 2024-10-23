c6c041cdf060b067de828235ae85aeb85cc2bedf
Carlsson111 <Linusjaa1@gmail.com>
2024-10-23 12:43:33 +0200

    Corrected the name in Main.java. Tested the codes functions. Seems Correct.

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -4,16 +4,18 @@ import static se.lexicon.NameRepository.*;
 
 public class Main {
     public static void main(String[] args) {
-        setNames(new String[]{"John Doe", "Jane Smith"});
+        setNames(new String[]{"Eric Svensson", "Jane Smith"});
         System.out.println("Number of elements in the array: " + getSize());
 
         System.out.println("All names: " + String.join(", ", findAll()));
 
-        System.out.println("Find 'John Doe': " + find("John Doe"));
+        System.out.println("Find 'Eric Svensson': " + find("Eric Svensson"));
         System.out.println("Find 'Alice Johnson': " + find("Alice Johnson"));
 
         System.out.println("Add 'Alice Johnson': " + add("Alice Johnson"));
         System.out.println("Add 'John Doe' again: " + add("John Doe"));
+        System.out.println("Add 'John Doe' again: " + add("John Doe"));
+
 
         System.out.println("All names after additions: " + String.join(", ", findAll()));
 
